26622ff77fe9c7b867b44ae7ba84654b1cd51590
ZeSved <timothy.korell@gmail.com>
2023-02-26 13:02:43 +0100

    updated image adding

--- src/util/manage.ts
+++ src/util/manage.ts
@@ -1,11 +1,12 @@
 import { Image } from "../main";
 import { closeImage } from "./image";
 
-const uploadImagePopup = document.querySelector('.upload-image-popup')
-const cancelUploadImageButton = document.querySelector('.cancel-upload-image-button')
-const uploadImageButton = document.querySelector('.upload-image-button')
-const imageURLInput = document.querySelector<HTMLInputElement>('.image-URL-input')!
+const uploadImagePopup = document.querySelector('.upload-image-popup__container')
+const cancelUploadImageButton = document.querySelector('.cancel')
+const uploadImageButton = document.querySelector('.upload')
+const imageURLInput = document.querySelector<HTMLInputElement>('.upload-image-popup__input')!
 const imageContainer = document.querySelector('.image-container')
+const fileImageUploadInput = document.querySelector<HTMLInputElement>('#file-image-upload__input')!
 
 cancelUploadImageButton?.addEventListener('click', cancelUploadImage)
 uploadImageButton?.addEventListener('click', uploadImage)
@@ -26,7 +27,14 @@ export function confirmUploadImage() {
 
 export function uploadImage() {
   const img = document.createElement('img')
-  img.src = imageURLInput.value
+
+  if (fileImageUploadInput.value !== '' && imageURLInput.value !== '') {
+    alert('You may only upload one file at a time.')
+    return
+  }
+
+  imageURLInput.value == '' ? img.src = fileImageUploadInput.value : img.src = imageURLInput.value
+
   img.id = JSON.stringify(Date.now())
 
   imageContainer?.appendChild(img)
@@ -36,4 +44,5 @@ export function uploadImage() {
 function toggleDisplayOnuploadImagePopup() {
   uploadImagePopup?.classList.toggle('display')
   imageURLInput.value = ''
+  fileImageUploadInput.value = ''
 }
